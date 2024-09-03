a72b7253c609d77b3c2750005cb56d7dedcef887
zarya25 <zarya.untouchable@mail.ru>
2024-09-03 15:50:36 +0300

    ptactica 1

---
+++ js/2zadanie.js
@@ -0,0 +1,9 @@
+'use strict';
+let firstname =  prompt('Введите вашу фамилию', 'uzer');
+alert(`Ваша фамилия ${firstname}`);
+let name =  prompt('Введите ваше имя', 'uzer');
+alert(`Ваше имя ${name}`);
+let lastname =  prompt('Введите ваше отчество', 'uzer');
+alert(`Ваше отчество ${lastname}`);
+let isTrue = confirm('Это действительно ваше фио?');
+alert(isTrue)
